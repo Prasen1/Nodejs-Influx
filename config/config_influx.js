1021151ce1c0bdc51c42dd56f407c0a9a4e5ec7e
Prasenjit Bhattacharjee <prasn94@gmail.com>
2021-12-01 13:59:06 +0530

    Add files via upload

---
+++ config/config_influx.js
@@ -0,0 +1,8 @@
+var configInflux = {
+    token: 'xxx',
+    organization: 'xxx',
+    bucket: 'xxx',
+    url: 'http://localhost:8086',
+    measurement_name: 'raw_data'
+}
+export default configInflux;
